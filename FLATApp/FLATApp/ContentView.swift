6c9e94e4f632afda64839a2f63d00d39c5112dce
user.email <b1019106@fun.ac.jp>
2021-10-09 02:44:03 +0900

    Fix: ファイル名を変更しました

---
+++ FLATApp/FLATApp/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  FLATApp
+//
+//  Created by 小田嶋　亜美 on 2021/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View{ //メイン画面
+    @State private var show: Bool = false
+    var body: some View {
+        Button(action: { self.show = true /*またはself.show.toggle() */ }) {
+            ZStack{
+                Circle()
+                    .frame(width: 80.0)
+                    .foregroundColor(Color(red: 0.2, green: 0.85, blue: 0.721))
+                Image(systemName: "person.badge.plus")
+                    .resizable()
+                    .frame(width: 50.0, height: 50.0, alignment: .leading)
+                    .foregroundColor(.white)
+            }
+        }
+        .sheet(isPresented: self.$show) {
+            IDsearchView()
+        }
+    }
+    
+}
+
+struct IDsearchView: View { //友達追加画面
+    @State private var number = ""
+    var body: some View {
+        VStack(){
+            VStack(){
+                Text("IDを入力してください")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(Color.gray)
+                    .padding(.leading, 25.0)
+                
+            }
+            HStack(){
+                TextField("000000", text: $number)
+                    .padding(3.0)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+        }
+        .padding(.top,139)
+        Spacer()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FLATApp/FLATApp/FLATAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLATAppApp.swift
+//  FLATApp
+//
+//  Created by 小田嶋　亜美 on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct FLATAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
