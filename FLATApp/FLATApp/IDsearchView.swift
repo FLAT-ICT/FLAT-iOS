234452c136cc4fd91e60f94a7258170033db2a2d
user.email <b1019106@fun.ac.jp>
2021-10-18 08:04:05 +0900

    Update: APIno

--- FLATApp/FLATApp/IDsearchView.swift
+++ FLATApp/FLATApp/IDsearchView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import UIKit
 
 
 struct IDsearchView: View { //友達追加画面
@@ -14,12 +14,28 @@ struct IDsearchView: View { //友達追加画面
     @Binding var isActive: Bool
     @State private var user: UserData = UserData(id: "", name: "",  icon_path: "",applied: false, requested: false)//通信用
     
-    struct UserData: Codable {
+    struct UserData: Decodable {
         var id: String
         var name: String
         var icon_path: String
         var applied: Bool
         var requested: Bool
+    }
+    enum NetworkError : Error{//失敗した時用
+        case unknown
+        case invalidResponse
+        case invalidURL
+    }
+    
+    func searchID(target_id: String){ //相手のIDを検索した時にそのIDをURLに入れる
+        guard let target_id_encode = target_id.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        guard let req_url = URL(string: "http://34.68.157.198:8080/v1/user/check?my_id=000000&target_id=\(target_id_encode)") else {
+            return
+        }
+        print(req_url)
     }
     
     var body: some View {
