6bd7dd2f9bbb1b91ba80d67c2fd235e351384ce5
user.email <b1019106@fun.ac.jp>
2021-10-18 08:29:44 +0900

    Update: API用のファイルにコードを移しました。

--- FLATApp/FLATApp/IDsearchView.swift
+++ FLATApp/FLATApp/IDsearchView.swift
@@ -14,31 +14,7 @@ struct IDsearchView: View { //友達追加画面
     @Binding var isActive: Bool
     @State private var user: UserData = UserData(id: "", name: "",  icon_path: "",applied: false, requested: false)//通信用
     
-    struct UserData: Decodable {
-        var id: String
-        var name: String
-        var icon_path: String
-        var applied: Bool
-        var requested: Bool
-    }
-    enum NetworkError : Error{//失敗した時用
-        case unknown
-        case invalidResponse
-        case invalidURL
-    }
-    
-    func searchID(target_id: String){ //相手のIDを検索した時にそのIDをURLに入れる
-        guard let target_id_encode = target_id.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            return
-        }
-        
-        guard let req_url = URL(string: "http://34.68.157.198:8080/v1/user/check?my_id=000000&target_id=\(target_id_encode)") else {
-            return
-        }
-        print(req_url)
-    }
-    
-    var body: some View {
+  var body: some View {
         VStack(){
             HStack(){ //上のキャンセルボタン
                 Button("キャンセル") {
