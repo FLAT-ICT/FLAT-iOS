f28230d81651e808b93676df2e3e578a215144fa
t4t5u0 <ymmtryk0902@gmail.com>
2021-10-19 23:37:53 +0900

    Update: APIの値をUIに流せるようにした

--- FLATApp/FLATApp/ApiView.swift
+++ FLATApp/FLATApp/ApiView.swift
@@ -20,19 +20,65 @@ enum NetworkError : Error{//失敗した時用
     case invalidURL
 }
 
-func searchID(target_id: String){ //相手のIDを検索した時にそのIDをURLに入れる
+
+func searchID(target_id: String,
+              success: @escaping (UserData) -> (),
+              failure: @escaping (Error) -> ()
+){ //相手のIDを検索した時にそのIDをURLに入れる
     guard let target_id_encode = target_id.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
         return
     }
     
     let req_url = "http://34.68.157.198:8080/v1/user/check?my_id=000000&target_id=\(target_id_encode)"
-    requestAsyncJson(urlString: req_url, success: {(dictionary) in
-        print(dictionary)
+    Api().checkUser(urlString: req_url, success: {(dictionary) in
+        success(dictionary)
     }) {(error) in
-        print(error)
+        failure(error)
     }
 }
 
+
+class Api{
+    
+    //    @Published var userData = UserData()
+    
+    func checkUser(
+        urlString: String,
+        success: @escaping (UserData) -> (),
+        failure: @escaping (Error) -> ()
+    ){
+        guard let url = URL(string: urlString) else {
+            return failure(NetworkError.invalidURL)
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request, completionHandler: {data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                failure(error)
+                return
+            }
+            
+            guard let data = data,
+                  let response = response as? HTTPURLResponse else {
+                      print("データまたはレスポンスがnil")
+                      failure(NetworkError.unknown)
+                      return
+                  }
+            if response.statusCode == 200 {
+                do {
+                    let object = try JSONDecoder().decode(UserData.self, from: data)
+                    // print(object["id"])
+                    success(object)
+                } catch let error {
+                    failure(error)
+                }
+            } else {
+                print("statusCode: \(response.statusCode)")
+                failure(NetworkError.unknown)
+            }
+        }).resume()
+    }
+    
     func requestAsyncJson(
         urlString: String,
         success: @escaping (Dictionary<String, Any>) -> (),
@@ -58,7 +104,7 @@ func searchID(target_id: String){ //相手のIDを検索した時にそのIDをU
                 if response.statusCode == 200 {
                     do {
                         let object = try JSONSerialization.jsonObject(with: data, options: []) as! Dictionary<String, Any>
-                        print(object["id"])
+                        // print(object["id"])
                         success(object)
                     } catch let error {
                         failure(error)
@@ -68,5 +114,5 @@ func searchID(target_id: String){ //相手のIDを検索した時にそのIDをU
                     failure(NetworkError.unknown)
                 }
             }).resume()
-    }
-
+        }
+}
