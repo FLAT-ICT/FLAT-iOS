60921f183077a5d44f9a09f23dd9cf55db3717b7
user.email <b1019106@fun.ac.jp>
2021-10-19 23:57:28 +0900

    Fix: リストの表示を変えました。

--- FLATApp/FLATApp/FriendListView.swift
+++ FLATApp/FLATApp/FriendListView.swift
@@ -11,58 +11,70 @@ struct FriendListView: View { //友達一覧画面
     @State private var show: Bool = false
     @State private var selection = 0
     var body: some View {
-        VStack{
-            VStack{
-                HStack{
-                    Text("友達が誰もいないようです。下のボタンから追加しましょう。")
-                }
-                .padding(.leading,24.0)
-                .padding(.trailing,24.0)
-            }
-            .padding(.top,107)
-          // VStack{
+        //VStack{
+            //VStack{
                 //HStack{
-                    //Text("未承認の友だち")
-                       // .underline(true, color: .green)
-                   // Spacer()
+                   // Text("友達が誰もいないようです。下のボタンから追加しましょう。")
                // }
-               // List(data){ item in
-                 //   NoFriendsDataView(data1: item)
-                    
-                //}
-                //.listStyle(PlainListStyle())
-                
-               // HStack{
-                 //   Text("友だち一覧")
-                   //     .underline(true, color: .green)
-                    //Spacer()
-                //}
-                //List(data2){ item in
-                  //  YesFriendsDataView(data2: item)
-                    
-               // }
-                //.listStyle(PlainListStyle())
+              //  .padding(.leading,24.0)
+               // .padding(.trailing,24.0)
            // }
-            
-            Button(action: { self.show = true /*またはself.show.toggle() */ }) {
-               
-                    Spacer()
-                    HStack {
+           // .padding(.top,107)
+       
+        ZStack{
+        List{
+            Section{
+                ForEach(Nofriends) { nofriends in
+                    HStack{
+                        Image(nofriends.icon_path)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text(nofriends.name)
                         Spacer()
-                        Image(systemName: "person.badge.plus")
-                            .foregroundColor(.white)
-                            .font(.system(size: 30))
-                            .frame(width: 60, height: 60)
-                            .background(Color(red: 0.2, green: 0.85, blue: 0.721))
-                            .cornerRadius(72.0)
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 107.0, trailing: 24.0))
+                        Text(nofriends.beacon)
                     }
-                
-                
+                }
+            } header: {
+                Text("未承認の友だち")
             }
-            .fullScreenCover(isPresented: self.$show) {
-                IDsearchView(isActive: $show)
+            Section{
+                ForEach(Yesfriends) { yesfriends in
+                    HStack{
+                    Image(yesfriends.icon_path)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text(yesfriends.name)
+                    Spacer()
+                    Text(yesfriends.beacon)
+                   
+                }
+                }
+            } header: {
+                Text("友だち一覧")
             }
+        }.listStyle(InsetListStyle())
+        VStack{
+            Spacer()
+                .frame(height: 633)
+        HStack{
+            Spacer()
+                .frame(width: 279)
+        Button(action: { self.show = true /*またはself.show.toggle() */ }) {
+           Image(systemName: "person.badge.plus")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                        .frame(width: 60, height: 60)
+                        .background(Color(red: 0.2, green: 0.85, blue: 0.721))
+                        .cornerRadius(72.0)
+                        
+            
+            
+        }
+        .fullScreenCover(isPresented: self.$show) {
+            //IDsearchView(isActive: $show)
+        }
+        }
+        }
         }
     }
     
