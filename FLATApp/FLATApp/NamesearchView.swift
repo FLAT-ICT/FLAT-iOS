7e7e6ac2f2382492e8bc002ad907cc3cc9388919
user.email <b1019106@fun.ac.jp>
2021-10-27 11:22:39 +0900

    Add: データのない名前を検索した場合の表示を追加しました

--- FLATApp/FLATApp/NamesearchView.swift
+++ FLATApp/FLATApp/NamesearchView.swift
@@ -11,13 +11,13 @@ import UIKit
 struct NamesearchView: View { //友達追加画面
     @State private var editting = false
     @Binding var isActive: Bool
-    @State private var users: [UserData] = []//通信用
+    @State private var users: [UserData] = []//通信用 UserData型の空配列
     @State var target_name: String = ""
     @State private var buttonText = "申請"
     @State private var buttonchange = false
     @State private var showError = false//バリデーションチェック
     @State private var errorMessage = "" //バリデーションチェックのためのエラーメッセージ
-    
+    @State private var counter = 0 //検索ボタンを押した回数
     var body: some View {
         VStack(){
             HStack(){ //上のキャンセルボタン
@@ -44,10 +44,12 @@ struct NamesearchView: View { //友達追加画面
                         
                         searchName(target_name: target_name,
                                    success: {(userData) in self.users = userData
-                        }
+                            }
                         ) { (error) in
                             print(error)
                         }
+                        self.countup()
+                        print(counter)
                     })
                         .padding(3.0)
                         .keyboardType(.default)
@@ -59,7 +61,6 @@ struct NamesearchView: View { //友達追加画面
                     .foregroundColor(Color.red)
             }
             VStack{
-                
                 ForEach(users){ user in
                     Text(user.name)
                     Text(user.icon_path)
@@ -73,6 +74,11 @@ struct NamesearchView: View { //友達追加画面
                                 .background(Color(red: 0.2, green: 0.85, blue: 0.721))
                                 .cornerRadius(24)
                         }
+                   
+                    }
+                if counter > 0 && users.isEmpty{
+                    Text("見つかりませんでした")
+                    Text("もう一度検索してください")
                 }
                 
             }
@@ -80,7 +86,7 @@ struct NamesearchView: View { //友達追加画面
         .padding(.top,139)
         Spacer()
     }
-    
+   
     
     func getData() {
         guard let url = URL(string: "http://34.68.157.198:8080/") else { return }
@@ -96,14 +102,15 @@ struct NamesearchView: View { //友達追加画面
                 print("Error", error)
             }
         }.resume()
-        
-        
     }
     private func validateName() {//バリデーションチェック
         if   self.target_name.isEmpty || self.target_name.count > 10 {
             self.errorMessage = "１０文字以内の名前を入力してください"
             self.showError = true
         }
+    }
+    func countup(){//検索ボタンの回数を数える関数
+        self.counter += 1
     }
 }
 
