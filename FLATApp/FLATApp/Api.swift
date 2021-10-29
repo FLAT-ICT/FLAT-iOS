2d0d861ff7df5021097b871be5ba61eafc4f0fa2
user.email <b1019106@fun.ac.jp>
2021-10-29 16:05:45 +0900

    Add: POST用のコードを追加しました。

--- FLATApp/FLATApp/Api.swift
+++ FLATApp/FLATApp/Api.swift
@@ -38,12 +38,22 @@ func searchName(target_name: String,
     }
 }
 
+func postNamesearch(target_name: String,
+                    success: @escaping ([UserData]) -> (),
+                    failure: @escaping (Error) -> ()){//申請ボタンを押した時に友人の名前をPOSTする
+    let req_url = "http://34.68.157.198:8080/"
+    Api().postfriends(urlString: req_url, success: {(dictionary) in
+        success(dictionary)
+    }) {(error) in
+        failure(error)
+    }
+}
 
 class Api{
     
     //    @Published var userData = UserData()
     
-    func checkUser(
+    func checkUser(//GET用
         urlString: String,
         success: @escaping ([UserData]) -> (),
         failure: @escaping (Error) -> ()
@@ -52,6 +62,44 @@ class Api{
             return failure(NetworkError.invalidURL)
         }
         let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request, completionHandler: {data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                failure(error)
+                return
+            }
+            
+            guard let data = data,
+                  let response = response as? HTTPURLResponse else {
+                      print("データまたはレスポンスがnil")
+                      failure(NetworkError.unknown)
+                      return
+                  }
+            if response.statusCode == 200 {
+                do {
+                    let object = try JSONDecoder().decode([UserData].self, from: data)
+                    // print(object["id"])
+                    success(object)
+                } catch let error {
+                    failure(error)
+                }
+            } else {
+                print("statusCode: \(response.statusCode)")
+                failure(NetworkError.unknown)
+            }
+        }).resume()
+    }
+    
+    func postfriends(//POST用
+        urlString: String,
+        success: @escaping ([UserData]) -> (),
+        failure: @escaping (Error) -> ()
+    ){
+        guard let url = URL(string: urlString) else {
+            return failure(NetworkError.invalidURL)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         URLSession.shared.dataTask(with: request, completionHandler: {data, response, error in
             if let error = error {
                 print(error.localizedDescription)
