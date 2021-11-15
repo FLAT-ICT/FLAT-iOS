8044ee3cb7fda7718cd63edf66989b2e9377498d
user.email <b1019106@fun.ac.jp>
2021-11-16 00:28:41 +0900

    Update: ビーコン検知の処理を実装用に書き換えました

--- FLATApp/FLATApp/SearchBeacon.swift
+++ FLATApp/FLATApp/SearchBeacon.swift
@@ -10,50 +10,94 @@ import CoreLocation
 import Combine
 
 class SearchBeacon: CLLocationManager, CLLocationManagerDelegate{
-    let defaultUUID = "E8C65602-6D9C-44EF-9734-B2D3EF1CD961" //自分が持っているiBeaconのUUIDを指定
+    //let defaultUUID = "E8C65602-6D9C-44EF-9734-B2D3EF1CD961" //自分が持っているiBeaconのUUIDを指定
     var locationManager = CLLocationManager()
-
     var beaconConstraints = [CLBeaconIdentityConstraint: [CLBeacon]]()
     var beacons = [CLProximity: [CLBeacon]]()
+    var isBeaconRange :Bool = false
+    let uuid = "E8C65602-6D9C-44EF-9734-B2D3EF1CD961"//自分のiBeaconを指定
+    var majorBeacon = Int()
+    var minorBeacon = Int()
+    var rssiBeacon = Float()
+    //指定したUUIDを出す処理(タイムホルダーをスタートした時に指定したビーコンのUUID、メジャー、マイナー、RSSIを表示する）
     
-//指定したUUIDを出す処理(タイムホルダーをスタートした時に指定したビーコンのUUID、メジャー、マイナー、RSSIを表示する）
-   
-
-
-
-func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
-    let beaconRegion = region as? CLBeaconRegion
-    if state == .inside {
-        // Start ranging when inside a region.
-        manager.startRangingBeacons(satisfying: beaconRegion!.beaconIdentityConstraint)
-    } else {
-        // Stop ranging when not inside a region.
-        manager.stopRangingBeacons(satisfying: beaconRegion!.beaconIdentityConstraint)
-    }
-
-
-/// - Tag: didRange
-func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
-    /*
-     Beacons are categorized by proximity. A beacon can satisfy
-     multiple constraints and can be displayed multiple times.
-     */
-    beaconConstraints[beaconConstraint] = beacons
-    
-    self.beacons.removeAll()
-    
-    var allBeacons = [CLBeacon]()
-    
-    for regionResult in beaconConstraints.values {
-        allBeacons.append(contentsOf: regionResult)
-    }
-    
-    for range in [CLProximity.unknown, .immediate, .near, .far] {
-        let proximityBeacons = allBeacons.filter { $0.proximity == range }
-        if !proximityBeacons.isEmpty {
-            self.beacons[range] = proximityBeacons
+    init(uuids: [String]) {
+        super.init()
+        locationManager.delegate = self
+        
+        for uuid in uuids {
+            self.monitoringBeacon(setUUID: uuid)
         }
     }
-}
-}
-}
+    func monitoringBeacon (setUUID: String) {
+        
+        if let uuid = UUID(uuidString: setUUID) {
+            self.locationManager.requestWhenInUseAuthorization() //アプリ使用中許可
+            let constraint = CLBeaconIdentityConstraint(uuid: uuid) //特定したビーコンの制約
+            self.beaconConstraints[constraint] = []  //制約を保存する配列
+            let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: setUUID)
+            //CLbeaconRegion < CLRegionより代入できる
+            self.locationManager.startMonitoring(for: beaconRegion)
+        }
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        let beaconRegion = region as? CLBeaconRegion
+        if state == .inside {
+            // Start ranging when inside a region.
+            print("検知中")
+            manager.startRangingBeacons(satisfying: beaconRegion!.beaconIdentityConstraint)
+        } else {
+            // Stop ranging when not inside a region.
+            print("検知停止")
+            manager.stopRangingBeacons(satisfying: beaconRegion!.beaconIdentityConstraint)
+        }
+        
+        
+        /// - Tag: didRange
+        func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+            /*
+             Beacons are categorized by proximity. A beacon can satisfy
+             multiple constraints and can be displayed multiple times.
+             */
+            beaconConstraints[beaconConstraint] = beacons
+            
+            self.beacons.removeAll()
+            
+            var allBeacons = [CLBeacon]()
+            
+            for regionResult in beaconConstraints.values {
+                allBeacons.append(contentsOf: regionResult)
+            }
+            
+            for range in [CLProximity.unknown, .immediate, .near, .far] {
+                let proximityBeacons = allBeacons.filter { $0.proximity == range }
+                if !proximityBeacons.isEmpty {
+                    self.beacons[range] = proximityBeacons
+                }
+            }
+            
+            for distance in allBeacons{
+                       //位置情報許可
+                       let accuracyBeacons = distance.accuracy
+                       let rssiBeacon = distance.rssi
+                       let doubleAccuracyBeacons: Double = Double(accuracyBeacons)
+                       let stringAccuracyBeacons = String(format: "accuracy: %.3f m", doubleAccuracyBeacons)
+                       let uuidBeacons = distance.uuid
+                       majorBeacon = Int(distance.major.intValue)
+                       minorBeacon = Int(distance.minor.intValue)
+                       print("major: \(majorBeacon)")
+                       print("miner: \(minorBeacon)")
+                       print("rssi: \(rssiBeacon)")
+                       if distance.uuid.uuidString == uuid {
+                           isBeaconRange = true
+                       }else{
+                           isBeaconRange = false
+                       }
+                   }
+               }
+        }
+    }
+
