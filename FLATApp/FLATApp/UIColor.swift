acd49c5bfed8ea69fccca0c99bb7971d6eb09144
t4t5u0 <ymmtryk0902@gmail.com>
2021-11-19 02:04:23 +0900

    Wip

---
+++ FLATApp/FLATApp/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  FLATApp
+//
+//  Created by t4t5u0 on 2021/11/18.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customColor = Color("CustomColor")
+}
