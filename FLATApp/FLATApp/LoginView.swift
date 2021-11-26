0430705013d7f1424cdbecb8ee0d989fbf29be71
user.email <b1019106@fun.ac.jp>
2021-11-27 00:41:46 +0900

    Add: ログイン画面用の新しいファイルを作りました。

---
+++ FLATApp/FLATApp/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FLATApp
+//
+//  Created by 小田嶋　亜美 on 2021/11/27.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
