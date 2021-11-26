8d7d8526876607e3ec6383370f38b5b69d52cf67
user.email <b1019106@fun.ac.jp>
2021-11-27 01:38:18 +0900

    Add: パスワードを入力する欄と新規登録画面に遷移するボタンを作りました。

--- FLATApp/FLATApp/LoginView.swift
+++ FLATApp/FLATApp/LoginView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct LoginView: View {
     @State var nickname: String = ""
+    @State var paseword: String = ""
     var body: some View {
         VStack(){
             VStack(){
@@ -25,6 +26,33 @@ struct LoginView: View {
                     .padding(3.0)
                     .keyboardType(.default)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                Text("パスワードを入力してください")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(Color.gray)
+                    .padding(.leading, 25.0)
+                    .padding(.top,18)
+                TextField("パスワード", text: $paseword)
+                    .padding(3.0)
+                    .keyboardType(.default)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: {}){
+                    Text("ログインする")
+                        .frame(width:327, height: 40)
+                        .foregroundColor(Color.white)
+                        .background(
+                            (Color("primary"))
+                        )
+                        .cornerRadius(10, antialiased: true)
+                }
+                .padding(.top, 32)
+                HStack(){
+                    Text("新規登録は")
+                    Button(action: {}){
+                        Text("こちら")
+                            .foregroundColor(Color("primary"))
+                    }
+                }
+                .padding(.top, 241)
             }
             .padding(.leading,24.0)
             .padding(.trailing,24.0)
