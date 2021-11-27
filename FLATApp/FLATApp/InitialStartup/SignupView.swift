c1d51692eba829197ee9034a6368cbd06d940ce4
user.email <b1019106@fun.ac.jp>
2021-11-28 01:10:01 +0900

    Update: テキストボックスの大きさを調節しました。

--- FLATApp/FLATApp/InitialStartup/SignupView.swift
+++ FLATApp/FLATApp/InitialStartup/SignupView.swift
@@ -12,64 +12,162 @@ struct SignupView: View {
     @State var firstPassword: String = ""
     @State var secondPassword : String = ""
     var body: some View {
+//        VStack(){
+//            VStack(){
+//                Text("アカウントを登録しましょう")
+//                    .font(.title)
+//                    .padding(.top, 115)
+//            }
+//            VStack(){
+//                Text("ニックネームを入力してください")
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .foregroundColor(Color.gray)
+//                    .padding(.leading, 25.0)
+//                TextField("ニックネーム", text: $nickname)
+//                    .padding(3.0)
+//                    .keyboardType(.default)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                Text("パスワードを入力してください")
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .foregroundColor(Color.gray)
+//                    .padding(.leading, 25.0)
+//                    .padding(.top,18)
+//                TextField("パスワード", text: $firstPassword)
+//                // 入力中の文字を隠す
+//                    .padding(3.0)
+//                    .keyboardType(.default)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                Text("パスワードを再度入力してください")
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .foregroundColor(Color.gray)
+//                    .padding(.leading, 25.0)
+//                    .padding(.top,18)
+//                TextField("パスワード", text: $secondPassword)
+//                // 入力中の文字を隠す
+//                    .padding(3.0)
+//                    .keyboardType(.default)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                Button(action: {}){//ホーム画面に画面遷移
+//                    Text("登録する")
+//                        .frame(width:327, height: 40)
+//                        .foregroundColor(Color.white)
+//                        .background(
+//                            (Color("primary"))
+//                        )
+//                        .cornerRadius(10, antialiased: true)
+//                }
+//                .padding(.top, 32)
+//                HStack(){
+//                    Text("アカウントを持っている人は")
+//                    Button(action: {}){//ログイン画面に画面遷移
+//                        Text("こちら")
+//                            .foregroundColor(Color("primary"))
+//                    }
+//                }
+//                .padding(.top, 200)
+//            }
+//            .padding(.leading,24.0)
+//            .padding(.trailing,24.0)
+//            .padding(.top, 60)
+//            Spacer()
+//        }
+//    }
         VStack(){
+            Text("アカウントを登録しましょう")
+                .font(.title)
+                //.padding(.top, 50)
             VStack(){
-                Text("アカウントを登録しましょう")
-                    .font(.title)
-                    .padding(.top, 115)
-            }
-            VStack(){
-                Text("ニックネームを入力してください")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(Color.gray)
-                    .padding(.leading, 25.0)
-                TextField("ニックネーム", text: $nickname)
-                    .padding(3.0)
-                    .keyboardType(.default)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("パスワードを入力してください")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(Color.gray)
-                    .padding(.leading, 25.0)
-                    .padding(.top,18)
-                TextField("パスワード", text: $firstPassword)
-                // 入力中の文字を隠す
-                    .padding(3.0)
-                    .keyboardType(.default)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("パスワードを再度入力してください")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(Color.gray)
-                    .padding(.leading, 25.0)
-                    .padding(.top,18)
-                TextField("パスワード", text: $secondPassword)
-                // 入力中の文字を隠す
-                    .padding(3.0)
-                    .keyboardType(.default)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Button(action: {}){//ホーム画面に画面遷移
-                    Text("登録する")
-                        .frame(width:327, height: 40)
-                        .foregroundColor(Color.white)
-                        .background(
-                            (Color("primary"))
-                        )
-                        .cornerRadius(10, antialiased: true)
-                }
-                .padding(.top, 32)
-                HStack(){
-                    Text("アカウントを持っている人は")
-                    Button(action: {}){//ログイン画面に画面遷移
-                        Text("こちら")
-                            .foregroundColor(Color("primary"))
-                    }
-                }
-                .padding(.top, 200)
+                NicknameView()
+                    .padding(.top, 32)
+                FirstPaswordView()
+                    .padding(.top, 18)
+                SecondPaswordView()
+                    .padding(.top, 18)
+                AccountButtonView()
+                    .padding(.top, 32)
             }
             .padding(.leading,24.0)
             .padding(.trailing,24.0)
-            .padding(.top, 60)
-            Spacer()
+            AccountMessageView()
+                .padding(.top, 200)
+            
+            
+            //.padding(.top, 60)
+            //Spacer()
+        }
+    }
+}
+struct NicknameView: View{
+    @State var nickname: String = ""
+    var body: some View{
+        VStack(){
+            Text("ニックネームを入力してください")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(Color.gray)
+            TextField("ニックネーム", text: $nickname)
+               // .padding(3.0)
+                .keyboardType(.default)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
+
+struct FirstPaswordView: View{
+    @State var firstPassword: String = ""
+    var body: some View{
+        VStack(){
+            Text("パスワードを入力してください")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(Color.gray)
+                //.padding(.top,18)
+            TextField("パスワード", text: $firstPassword)
+            // 入力中の文字を隠す
+                //.padding(3.0)
+                .keyboardType(.default)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
+
+struct SecondPaswordView: View{
+    @State var secondPassword : String = ""
+    var body: some View{
+        VStack(){
+            Text("パスワードを再度入力してください")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(Color.gray)
+            TextField("パスワード", text: $secondPassword)
+            // 入力中の文字を隠す
+                //.frame(maxWidth: .infinity,maxHeight: 40)
+                .cornerRadius(10, antialiased: true)
+                .keyboardType(.default)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
+
+struct AccountButtonView: View{
+    var body: some View{
+        Button(action: {}){//ホーム画面に画面遷移
+            Text("登録する")
+                .frame(maxWidth:.infinity, maxHeight: 40)
+                .foregroundColor(Color.white)
+                .background(
+                    (Color("primary"))
+                )
+                .cornerRadius(10, antialiased: true)
+        }
+    }
+}
+
+struct AccountMessageView: View{
+    var body: some View{
+        HStack(){
+            Text("アカウントを持っている人は")
+            Button(action: {}){//ログイン画面に画面遷移
+                Text("こちら")
+                    .foregroundColor(Color("primary"))
+            }
         }
     }
 }
